78474a064faeea1e34ee18fdb61d99cb15760b94
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-07-16 14:59:05 +0000

    flaot

--- Calcula_Media_Nota/Programa_Calc_nota.c
+++ Calcula_Media_Nota/Programa_Calc_nota.c
@@ -3,13 +3,10 @@
 
 int main(){
 
-   float nota1;
-
-   float nota2;
-
-   float nota3;
+   float nota1, nota2, nota3;
 
    float media;
+   
 
     printf("Digite a primera nota\n");
     scanf("%f",&nota1);
